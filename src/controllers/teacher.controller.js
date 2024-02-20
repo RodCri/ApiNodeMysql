0f23a7631b193fc477ad8817cc48149ec090e39d
RodCri <cristian.rodriguez1317@gmail.com>
2024-02-20 16:56:54 -0500

    add controllers with logic

---
+++ src/controllers/teacher.controller.js
@@ -0,0 +1,7 @@
+export const getTeachers = (req,res) => res.send('get teacher...');
+
+export const addTeacher = (req,res) => res.send('create teacher...');
+
+export const updateTeacher = (req,res) => res.send('update teacher...');
+
+export const deleteTeacher = (req,res) => res.send('delete teacher...');
