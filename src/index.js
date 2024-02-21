bdd12ff81986e24d56f600c26f5d68e5c965944d
RodCri <cristian.rodriguez1317@gmail.com>
2024-02-21 09:54:33 -0500

    Add route 404 and dotenv

--- src/index.js
+++ src/index.js
@@ -12,5 +12,11 @@ app.use(express.json())
 app.use('/api',routerTeacher);
 app.use(pingRoute);
 
+app.use((req,res,next) =>{
+  res.status(404).json({
+    message: 'EndPoint not found'
+  })
+})
+
 app.listen(5000)
 console.log("server listening port 5000");
